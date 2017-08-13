452ed2a17d02867d50664a6adc3fd5127c5aad1e
Benjamin Caradeuc <bencrdc@gmail.com>
2017-08-14 01:04:19 +0200

    allow origin *

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(function(err, req, res, next) {
     res.status(err.status || 500)
         .json({
