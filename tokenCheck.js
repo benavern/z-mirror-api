6f3e601fce2e7904fbf7d1a14af2b8243501ac84
Benjamin Caradeuc <bencrdc@gmail.com>
2017-09-04 23:38:56 +0200

    [WIP] token

--- tokenCheck.js
+++ tokenCheck.js
@@ -1,16 +1,17 @@
 module.exports = function(req, res, next) {
-    const securised = req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE'
+    const secured = req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE'
     const token = process.env.z_mirror_token || null
     const received = req.headers.z_mirror_token || null
 
-    if (!securised || token && received && token === received) {
+    if (!secured || token && received && token === received) {
         next()
     } else {
         res.status(401).json({
             status: 'error',
             message: 'You are not allowed to modify data here!',
             headers: req.headers,
-            method: req.method
+            method: req.method,
+            secured
         })
     }
 }
