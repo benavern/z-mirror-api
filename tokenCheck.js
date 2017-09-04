86198906ecee9c34ebb3143657e49cfcf1307285
Benjamin Caradeuc <bencrdc@gmail.com>
2017-09-04 23:27:45 +0200

    [WIP] token

--- tokenCheck.js
+++ tokenCheck.js
@@ -8,7 +8,8 @@ module.exports = function(req, res, next) {
     } else {
         res.status(401).json({
             status: 'error',
-            message: 'You are not allowed to modify data here!'
+            message: 'You are not allowed to modify data here!',
+            headers: req.headers
         })
     }
 }
